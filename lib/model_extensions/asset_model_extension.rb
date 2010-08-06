236e345ba31fa39e4a69cbd05a9fe440bf89e5df
Christopher Rankin <rankin.devon@gmail.com>
2010-08-06 14:22:25 -0400

    Added has_many gallery_items, :dependent => :destroy to fix deleting an asset with paperclip failing.

--- lib/model_extensions/asset_model_extension.rb
+++ lib/model_extensions/asset_model_extension.rb
@@ -3,6 +3,7 @@ module AssetGallery
     def self.included(base)
       base.class_eval {
         has_many  :galleries, :dependent => :destroy
+	has_many :gallery_items, :dependent => :destroy
       }
     end
   end
